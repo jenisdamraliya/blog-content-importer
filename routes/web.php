d51bdbff9f3fe9c3c4aad5c6d74c460f5f92bef9
JenishDamraliya <damraliyajenish74@gmail.com>
2025-11-04 14:36:09 +0530

    Fixed Login & Register page routes issue

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,10 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+Route::view('/login', 'auth.login')->name('login');
+Route::view('/register', 'auth.register')->name('register');
+Route::post('/logout', function () {
+    auth()->logout();
+    return redirect('/');
+})->name('logout');
